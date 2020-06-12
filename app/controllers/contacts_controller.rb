25e3e099c98ca453a6703c88c642859c1bd57f9c
Daniel Constantine <dconstantine86@gmail.com>
2020-06-12 05:00:35 +0000

    added mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,10 @@ end
 def create
   @contact = Contact.new(contact_params)
   if @contact.save
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    body = params[:contact][:body]
+    ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent."
      redirect_to contact_us_path
   else
