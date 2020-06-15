0beb5abf159dbd79ac95cc28186170ef3f094a70
Daniel Constantine <dconstantine86@gmail.com>
2020-06-15 08:41:16 +0000

    Added bootstrap to user reg

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-    
+    # Contact form validations
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
